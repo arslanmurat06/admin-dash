f59554cd5462993418b9402131f620aafc12e10b
Murat Arslan <muratarslan@Murats-Air.Dlink>
2022-05-30 11:12:07 +0300

    first implementation

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+const Widget = () => {
+  return (
+    <div className="widget">
+      <div className="left">
+        {" "}
+        <span className="title">USERS</span>
+        <span className="counter">120</span>
+        <span className="link">See all users</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          20%
+        </div>
+        <PersonOutlinedIcon className="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
+import "./home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget />
+          <Widget />
+          <Widget />
+          <Widget />
+        </div>
+        home container
+      </div>
+    </div>
+  );
+}
+
+export default Home;
